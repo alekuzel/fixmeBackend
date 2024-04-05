9e3e45ba931d66199bcfaade3fdbdbb7532c7440
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-04-05 14:13:08 +0200

    authmiddleware

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 // Import controllers
 const usersController = require('./controllers/usersController');
 const adminsController = require('./controllers/adminsController');
-// Import other controllers as needed
+const authenticateAdmin = require('./middleware/authMiddleware'); // Import authMiddleware
 
 const app = express();
 const port = 3006;
@@ -14,17 +14,16 @@ app.use(bodyParser.json());
 // Mount controllers to specific routes
 app.use('/users', usersController);
 app.use('/admins', adminsController);
-// Mount other controllers as needed
 
+// Example route with admin authentication
 app.post('/admin/login', authenticateAdmin, (req, res) => {
     res.json({ message: 'Authentication successful', admin: req.admin });
 });
 
-//protected route
+// Example protected route
 app.get('/admin/dashboard', authenticateAdmin, (req, res) => {
     res.json({ message: 'Admin dashboard', admin: req.admin });
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
